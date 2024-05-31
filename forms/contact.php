0e8a15945cfa43a8716af2655d92ab77ebbecb9d
roro1839 <ronaldokipkirui90@gmail.com>
2024-05-31 11:50:16 +0300

    add new form

--- forms/contact.php
+++ forms/contact.php
@@ -6,35 +6,48 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'ronaldokipkirui90@gmail.com';
+  // Use environment variables or configuration files for sensitive data
+  $receiving_email_address = getenv('RECEIVING_EMAIL_ADDRESS');
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+  if (!$receiving_email_address) {
+    die('Receiving email address is not set.');
+  }
+
+  if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die('Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
+  // Validate and sanitize POST inputs
+  $contact->from_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+  $contact->from_email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+  $contact->subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+
+  if (!$contact->from_email) {
+    die('Invalid email address.');
+  }
+
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
-    'username' => 'ronaldokipkirui90@gmail.com',
-    'password' => 'Rn#11839',
+    'username' => getenv('SMTP_USERNAME'),
+    'password' => getenv('SMTP_PASSWORD'),
     'port' => '587'
   );
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  if (!$contact->smtp['username'] || !$contact->smtp['password']) {
+    die('SMTP credentials are not set.');
+  }
+
+  $contact->add_message($contact->from_name, 'From');
+  $contact->add_message($contact->from_email, 'Email');
+  $contact->add_message(filter_var($_POST['message'], FILTER_SANITIZE_STRING), 'Message', 10);
 
   echo $contact->send();
 ?>
